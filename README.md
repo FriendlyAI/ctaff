937a3d49924f962208e9139dc98675a89b2f830b
Andrew <li_andrewy@protonmail.com>
2020-02-14 15:44:40 -0500

    Better detection timing and more command line control

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # ctaff
+An automated rhythm game map generator written in pure C.
+
+### Setup
+Run `make` in `ctaff/` subfolder.
+
+### Usage
+Run `ctaff/bin/ctaff -i "<INPUT FILEPATH> -o <OUTPUT FILEPATH>"` or `ctaff/bin/ctaff` if a raw PCM file is already in `tmp/tmp.raw`. The PCM file must be in mono 32-bit float little-endian format.
 
 ### Dependencies
 [KISS FFT](https://github.com/mborgerding/kissfft)
--- ctaff/ctaff.c
+++ ctaff/ctaff.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "lib/kissfft/kiss_fftr.h"
 
@@ -52,13 +53,16 @@ bool detect_beat(double average, double maximum, double average_increase_ratios[
         return true;
     }
     /* average */
-    else if (maximum > 200 && average_increase_ratios[0] > 1.4 && average_increase_ratios[1] > 1.3) {
+    // else if (maximum > 200 && average_increase_ratios[0] > 1.5 && average_increase_ratios[1] > 1.4) {
+    //     return true;
+    // }
+    else if (maximum > 250 && average > 100 && average_increase_ratios[0] > 1.4 && average_increase_ratios[1] > 1.3) {
         return true;
     }
-    else if (maximum > 250 && average_increase_ratios[0] > 1.3 && average_increase_ratios[1] > 1.2) {
+    else if (maximum > 300 && average > 100 && average_increase_ratios[0] > 1.2 && average_increase_ratios[1] > 1.1) {
         return true;
     }
-    else if (maximum > 300 && average_increase_ratios[0] > 1.2 && average_increase_ratios[1] > 1.1) {
+    else if (maximum > 350 && average > 100 && average_increase_ratios[0] > 1.1 && average_increase_ratios[1] > 1.1) {
         return true;
     }
     /* maximum */
@@ -80,30 +84,36 @@ bool detect_beat(double average, double maximum, double average_increase_ratios[
 
 void detect_bass(kiss_fft_cpx *out, BeatList_t *beats, float time, BassVariables_t *bass_variables) {
     bool detected = false;
+    // if (time > 62 && time < 63)
+    // printf("******************* %f seconds *******************\n", time);
     // do higher frequencies
     double average = 0, maximum = 0;
     for (int i = 0; i < 11; i++) {
         double magnitude = sqrt(out[i].r * out[i].r + out[i].i * out[i].i);
-        // printf("%f | %f\n", i*44100/(float)nfft, magnitude);
+        // if (time > 62 && time < 63)
+        // printf("%f | %f\n", i*44100/2048., magnitude);
         average += magnitude;
         if (magnitude > maximum)
             maximum = magnitude;
     }
     average /= 11.;
+    // if (time > 62 && time < 63)
     // printf("MAXIMUM: %f\nAVERAGE: %f\n", maximum, average);
     double average_increase_ratios[2] = {average / bass_variables->last_averages_high[0], average / bass_variables->last_averages_high[1]},
            maximum_increase_ratios[2] = {maximum / bass_variables->last_maximums_high[0], maximum / bass_variables->last_maximums_high[1]};
 
-    if (detect_beat(average, maximum, average_increase_ratios, maximum_increase_ratios)) {
-        if (!bass_variables->last_was_detected[0] && !bass_variables->last_was_detected[1]) {
+    
+    if (!bass_variables->last_was_detected[0] && !bass_variables->last_was_detected[1]) {
+        if (detect_beat(average, maximum, average_increase_ratios, maximum_increase_ratios)) {
             add_beat(beats, time, 'A');
             detected = true;
         }
-        else if (bass_variables->last_was_detected[0] && average_increase_ratios[0] > 1.2 && maximum_increase_ratios[0] > 1.2) {
-            beats->tail->time = (beats->tail->time + time) / 2.;
-            beats->tail->layer = 'C';
-            detected = true;
-        }
+    }
+    else if (beats->tail->layer == 'A' && (average_increase_ratios[0] > 1.1 || maximum_increase_ratios[0] > 1.1) && 
+                                          (average_increase_ratios[1] > 1.1 || maximum_increase_ratios[1] > 1.1)) {
+        beats->tail->time = (beats->tail->time + time) / 2.;
+        beats->tail->layer = 'C';
+        detected = true;
     }
 
     bass_variables->last_averages_high[1] = bass_variables->last_averages_high[0];
@@ -127,17 +137,28 @@ void detect_bass(kiss_fft_cpx *out, BeatList_t *beats, float time, BassVariables
     maximum_increase_ratios[0] = maximum / bass_variables->last_maximums_low[0];
     maximum_increase_ratios[1] = maximum / bass_variables->last_maximums_low[1];
 
-    if (!detected && detect_beat(average, maximum, average_increase_ratios, maximum_increase_ratios)) {
-        if (!bass_variables->last_was_detected[0] && !bass_variables->last_was_detected[1]) {
+    if (detect_beat(average, maximum, average_increase_ratios, maximum_increase_ratios)) {
+        if (!detected && !bass_variables->last_was_detected[0] && !bass_variables->last_was_detected[1]) {
             add_beat(beats, time, 'B');
             detected = true;
         }
-        else if (bass_variables->last_was_detected[0] && average_increase_ratios[0] > 1.2 && maximum_increase_ratios[0] > 1.2) {
+        else if (bass_variables->last_was_detected[0] && average_increase_ratios[0] > 1.1 && maximum_increase_ratios[0] > 1.1) {
             beats->tail->time = (beats->tail->time + time) / 2.;
             beats->tail->layer = 'D';
             detected = true;
         }
     }
+    // if (!detected && !bass_variables->last_was_detected[0] && !bass_variables->last_was_detected[1]) {
+    //     if (detect_beat(average, maximum, average_increase_ratios, maximum_increase_ratios)) {
+    //         add_beat(beats, time, 'B');
+    //         detected = true;
+    //     }
+    // }
+    // else if (bass_variables->last_was_detected[0] && beats->tail->layer == 'D' && (average_increase_ratios[0] > 1.1 || maximum_increase_ratios[0] > 1.1)) {
+    //     beats->tail->time = (beats->tail->time + time) / 2.;
+    //     beats->tail->layer = 'D';
+    //     detected = true;
+    // }
 
     bass_variables->last_averages_low[1] = bass_variables->last_averages_low[0];
     bass_variables->last_averages_low[0] = average;
@@ -155,21 +176,39 @@ void detect_bass(kiss_fft_cpx *out, BeatList_t *beats, float time, BassVariables
 }
 
 int main(int argc, char *argv[]) {
+    FILE *label_ptr = label_ptr = fopen("tmp/_labels.txt", "w+");
+    const float PI = 3.1415927;
+
     if (argc > 1) {
         system("mkdir -p tmp");
 
-        int buffer_size = 52+201; // 52 chars for command + 201 chars for filepath * 2
+        int opt;
+        while ((opt = getopt(argc, argv, "i:o:")) != -1) {
+            switch (opt) {
+                case 'i':
+                    {
+                        int buffer_size = 52+201; // 52 chars for command + 201 chars for filepath
 
-        char command_buffer[buffer_size]; 
-        int make_command = snprintf(command_buffer, buffer_size, "ffmpeg -i \"%1$s\" -y -ac 1 -f f32le -ar 44100 tmp%2$stmp.raw", argv[1], kPathSeparator);
-        if (make_command < 0 || make_command >= buffer_size) {
-            printf("Error: Can't decode filename. Remove non-ascii characters and ensure the file path is at most 200 characters long.");
-            return 1;
+                        char command_buffer[buffer_size]; 
+                        int make_command = snprintf(command_buffer, buffer_size, "ffmpeg -i \"%1$s\" -y -ac 1 -f f32le -ar 44100 tmp%2$stmp.raw", optarg, kPathSeparator);
+                        if (make_command < 0 || make_command >= buffer_size) {
+                            printf("Error: Can't decode filename. Remove non-ascii characters and ensure the file path is at most 200 characters long.");
+                            return 1;
+                        }
+                        system(command_buffer);
+                    }
+                    break;
+                case 'o':
+                    label_ptr = fopen(optarg, "w+");
+                    if (label_ptr == NULL) {
+                        printf("Error: output file can't be opened. Defaulting to tmp/_labels.txt");
+                        label_ptr = fopen("tmp/_labels.txt", "w+");
+                    }
+                    break;
+            }
         }
-        system(command_buffer);
     }
 
-    const float PI = 3.1415927;
 
     FILE *file_ptr;
 
@@ -177,13 +216,10 @@ int main(int argc, char *argv[]) {
     file_ptr = fopen(strcat(strcat(raw_filepath, kPathSeparator), "tmp.raw"), "rb"); // tmp/tmp.raw
 
     if (file_ptr == NULL) {
-        printf("Error: file can't be opened.");
+        printf("Error: no file specified or file can't be opened.");
         return 1;
     }
-
-    FILE *label_ptr;
-    label_ptr = fopen("tmp/_labels.txt", "w+");
-
+    
     float sample;
     const int nfft = 2048;
     kiss_fft_scalar in[nfft];
@@ -221,7 +257,6 @@ int main(int argc, char *argv[]) {
         kiss_fftr(fwd, &in[0], &out[0]);
 
         float time = (frame + 0.5) * nfft / 44100.;
-        // printf("******************* %f seconds *******************\n", (frame + .5) * nfft / 44100.);
         detect_bass(&out[0], beats, time, bass_variables);
 
         frame++;
@@ -236,8 +271,12 @@ int main(int argc, char *argv[]) {
     free(beats);
     kiss_fftr_free(fwd);
     
+    // Remove later
     char remove_tmp_command[15] = "rm ";
     system(strcat(remove_tmp_command, raw_filepath));
+
+    // if (argc > 1)
+    //     system('rm -r tmp')
 
     return 0;
 }
